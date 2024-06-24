e398162b4711f4509d7a712486cd9b53253e163a
Egor Belushkov <belushkov@nobr.ru>
2024-06-24 21:17:23 +0300

    Третья домашка

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,14 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const properties = path.split('.');
 
+  return function (obj) {
+    return properties.reduce((acc, prop) => {
+      if (acc === undefined) {
+        return undefined;
+      }
+      return acc[prop];
+    }, obj);
+  };
 }
